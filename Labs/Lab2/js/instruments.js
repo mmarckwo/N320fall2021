70c93c894c9145fe57eccaf7e05ee785b2dcc8d5
mmarckwo <78805766+mmarckwo@users.noreply.github.com>
2021-09-06 03:33:41 -0400

    Update instruments.js

--- Labs/Lab2/js/instruments.js
+++ Labs/Lab2/js/instruments.js
@@ -12,18 +12,25 @@ class Instruments {                             // Main instrument class.
 
 class Woodwind extends Instruments {            // Woodwind subclass.
     constructor(loudness) {                     // take in custom loudness amount.
-        super(loudness, "Woodwind", "play");    // pass up the loudness amount but set the family and verb. same for subclasses below.
+        super(loudness);                        // pass up the loudness amount.
+        this.family = "Woodwind";               // set family property.
+        this.verb = "play";                     // set verb property. same process for subclasses below.
+        
     }
 }
 
 class Percussion extends Instruments {          // Percussion subclass.
     constructor(loudness) {
-        super(loudness, "Percussion", "ting");
+        super(loudness)
+        this.family = "Percussion";
+        this.verb = "ting";
     }
 }
 
 class String extends Instruments {              // String subclass.
     constructor(loudness) {
-        super(loudness, "String", "resonate");
+        super(loudness)
+        this.family = "String";
+        this.verb = "resonate";
     }
 }
